98927545fccad21d09d89107f74ddf8b13c66461
PyeonSangJin <vustkd157157@naver.com>
2020-02-20 15:19:50 +0900

    fix lobby ui

---
+++ Client/Assets/Scripts/Item.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item 
+{
+    [SerializeField]
+    private int m_price;
+
+    [SerializeField]
+    private string m_name;
+
+    [SerializeField]
+    private string m_description;
+
+    public int Price {
+        get { return m_price; }
+        set { if (m_price != value) {
+                m_price = value;
+                //NotifyProperty("Price", value);
+            } 
+        }
+    }
+
+    public string Name {
+        get { return m_name; }
+        //set {
+        //    if (m_name != value) {
+        //        m_name = value;
+        //        NotifyProperty("Name", value);
+        //    }
+        //}
+    }
+
+    public string Description {
+        get { return m_description; }
+        //set
+        //{
+        //    if (m_description != value)
+        //    {
+        //        m_description = value;
+        //        NotifyProperty("Description", value);
+        //    }
+        //}
+    }
+}

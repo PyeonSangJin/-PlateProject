a18b58585e8225b9fd57bd92f9ba020073b5331f
PyeonSangJin <vustkd157157@naver.com>
2020-03-03 23:30:38 +0900

    add searchFunc

---
+++ Client/Assets/Scripts/PlateConfig.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlateConfig : MonoBehaviour
+{
+    private Button side1, side2, side3;
+    private Button bread, soup;
+    private GameObject searchEngine;
+
+    private void Awake()
+    {
+        side1 = GetComponent<Button>();
+        side2 = GetComponent<Button>();
+        side3 = GetComponent<Button>();
+        bread = GetComponent<Button>();
+        soup = GetComponent<Button>();
+        searchEngine = GameObject.Find("SearchEngine");
+        searchEngine.SetActive(false);
+    }
+
+    void Start()
+    {
+        side1 = GameObject.Find("side1").GetComponent<Button>();
+        side2 = GameObject.Find("side2").GetComponent<Button>();
+        side3 = GameObject.Find("side3").GetComponent<Button>();
+        bread =GameObject.Find("main").GetComponent<Button>();
+        soup = GameObject.Find("soup").GetComponent<Button>();
+
+        side1.onClick.AddListener(delegate () { SearchEngine(); });
+        side2.onClick.AddListener(delegate () { SearchEngine(); });
+        side3.onClick.AddListener(delegate () { SearchEngine(); });
+        bread.onClick.AddListener(delegate () { SearchEngine(); });
+        soup.onClick.AddListener(delegate () { SearchEngine(); });
+    }
+
+    public void SearchEngine() {
+        searchEngine.SetActive(true);
+    }
+}
---
+++ Client/Assets/Scripts/SearchConfig.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SearchConfig : MonoBehaviour
+{
+    private InputField search;
+
+    private void Awake()
+    {
+        //        side1 = GetComponent<Button>();
+        search = GameObject.Find("search").GetComponent<InputField>();
+    }
+
+    private void Start() {
+        search.onValueChanged.AddListener(TextUpdated);
+        //search.onEndEdit.AddListener(TextUpdated);
+    }
+
+    public void TextUpdated(string str) {
+        Debug.Log("OUTOUTOUTOUT : " + str);
+    }
+}

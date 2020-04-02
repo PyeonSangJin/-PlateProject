525d9f83edfd04a92a94170ce9aab69a2ecee464
PyeonSangJin <vustkd157157@naver.com>
2020-04-02 17:38:21 +0900

    fix error

--- Client/Assets/Scripts/MainHandler/LoadFood.cs
+++ Client/Assets/Scripts/MainHandler/LoadFood.cs
@@ -13,24 +13,29 @@ public class LoadFood : MonoBehaviour
         for (int i = 0; i < 5; i++)
         {
             try{
-                //Debug.Log(name[i]);
-                if (name[i] == null || name[i].Equals("") || name[i].Equals(null)) continue;
+                var name = ChangeScene.name[i];
+                if (name == null || name.Equals("") || name.Equals(null)) continue;
+
                 food = Resources.Load<GameObject>("Prefabs/" + ChangeScene.name[i]);
                 GameObject userImages = Instantiate(food);
+
+                //위치 크기 조절여기서 하면 됩니다.
+                //0번 side1
+                //1번 side2
+                //2번 side3
+                //3번 bread
+                //4번 soup
+
                 //userImages.transform.SetParent(b_obj.transform);
                 userImages.transform.localPosition = new Vector3(0, 0, 0);
                 //userImages.transform.localScale = new Vector3(b_obj.GetComponent<RectTransform>().sizeDelta.x, b_obj.GetComponent<RectTransform>().sizeDelta.y, 1);
+
             }catch (Exception e) {
-                Debug.Log(name[i] + ":is not available");
+                Debug.Log(name + ":is not available");
                 continue;                
             }
 
         }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
